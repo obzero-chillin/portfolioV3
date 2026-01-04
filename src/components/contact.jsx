29048027d6ce18b4c2e62c8dba88478918d3135f
Obonolo Modise <obonolomodd@gmail.com>
2026-01-04 13:10:08 +0200

    added contact section, has no logic yet

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,6 +6,18 @@ const Contact = () => {
     <div id='contact'>
         <h1>Contact</h1>
         <h2>Lets talk</h2>
+        <div id='contactFormContainer'>
+            <form>
+                <label htmlFor="name">Your Name</label><br></br>
+                <input name="name" className='inputField' /><br></br>
+                <label htmlFor="email">Your Email</label><br></br>
+                <input name="email" className='inputField'/><br></br>
+                <label htmlFor="message">Message</label><br></br>
+                <textarea name='message' className='inputField'></textarea><br></br>
+                <button id="submit" type='submit'>Send</button>
+            </form>
+
+        </div>
     </div>
   )
 }
